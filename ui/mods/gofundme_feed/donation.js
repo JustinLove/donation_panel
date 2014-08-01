5f330693d8b44d2585db6699031ac87d614b8eba
Justin Love <git@JustinLove.name>
2014-08-01 17:39:59 -0500

    minimum total for order

--- ui/mods/gofundme_feed/donation.js
+++ ui/mods/gofundme_feed/donation.js
@@ -8,6 +8,9 @@ define(function() {
     model.selected = ko.observable(false)
     model.orders = model.orders || []
     model.unexectedOrders = model.orders.concat()
+    model.minimum = model.orders
+      .map(function(o) {return o.donation})
+      .reduce(function(a, b) {return a + b}, 0)
     return model
   }
 
