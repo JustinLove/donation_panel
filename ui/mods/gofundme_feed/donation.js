57ab3597366aa7713e39442d55ec0c88cf069ced
Justin Love <git@JustinLove.name>
2014-08-01 17:47:24 -0500

    expand simple multiples

--- ui/mods/gofundme_feed/donation.js
+++ ui/mods/gofundme_feed/donation.js
@@ -11,6 +11,16 @@ define(function() {
     model.minimum = model.orders
       .map(function(o) {return o.donation})
       .reduce(function(a, b) {return a + b}, 0)
+    if (model.orders.length == 1) {
+      var credit = model.amount
+      var item = model.orders[0]
+      credit -= item.donation
+      while (credit >= item.donation) {
+        credit -= item.donation
+        model.minimum += item.donation
+        model.unexectedOrders.push(item)
+      }
+    }
     return model
   }
 
