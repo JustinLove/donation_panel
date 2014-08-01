08b9f832af3f6e33520d88ca9e28af71f55f1851
Justin Love <git@JustinLove.name>
2014-08-01 16:14:00 -0500

    more like planets panel, temporary position for start screen testing

--- ui/mods/gofundme_feed/panel.js
+++ ui/mods/gofundme_feed/panel.js
@@ -3,14 +3,14 @@ define(function() {
     var $panel = $('<panel id="gofundme_feed"></panel>').css({
       visibility: 'visible',
       position: 'absolute',
-      top: 0,
+      bottom: 0,
       left: 0,
     }).attr({
       name: "gofundme_feed",
       src: "coui://ui/mods/gofundme_feed/gofundme_feed.html",
       'no-keyboard': true,
       'yield-focus': true,
-      fit: "dock-top",
+      fit: "dock-bottom-right",
     })
     $panel.appendTo('body')
     api.Panel.bindElement($panel[0])
