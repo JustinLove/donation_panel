4d03eca9c916e355ccc7e04d41b493c420e7f70e
Justin Love <git@JustinLove.name>
2015-10-11 19:10:01 -0500

    better fundrazr url

--- ui/mods/donation_panel/fundrazr/live.js
+++ ui/mods/donation_panel/fundrazr/live.js
@@ -1,6 +1,6 @@
 define(['donation_panel/fundrazr/parse'], function(parse) {
-  var donations = "https://fundrazr.com/api/campaigns/811ws3/highlights?v=1&max-results=25&order=newest-first"
-  //var donations = "https://fundrazr.com/api/campaigns/4xZAc/highlights?v=1&max-results=25&order=newest-first"
+  var donations = "https://fundrazr.com/api/campaigns/811ws3/activities?v=1&max-results=25&order=newest-first"
+  //var donations = "https://fundrazr.com/api/campaigns/4xZAc/activities?v=1&max-results=25&order=newest-first"
 
   var update = function(url) {
     return $.getJSON(url || donations, {_: new Date().getTime()}).then(parse.process)
