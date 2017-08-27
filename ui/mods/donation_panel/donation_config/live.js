27541843c26c8e48a0d0d9f73bd784f3e03a43a6
Justin Love <git@JustinLove.name>
2017-08-27 16:24:48 -0500

    request game filtered feed from our server

--- ui/mods/donation_panel/donation_config/live.js
+++ ui/mods/donation_panel/donation_config/live.js
@@ -1,13 +1,19 @@
 define([
   'donation_panel/donation_config/parse',
-], function(parse) {
+  'donation_panel/config',
+], function(parse, config) {
   var local = "http://localhost:5100/donations"
   var donations = local
+
+  var query = ""
+  if (config.current_match()) {
+    "?untagged=true&game=" + config.current_match()
+  }
 
   var update = function(url) {
     return $.ajax({
       method: 'GET',
-      url: url || donations,
+      url: (url || donations) + query,
       dataType: 'json',
     }).then(parse.process)
   }
